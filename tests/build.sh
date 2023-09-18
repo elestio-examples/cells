93103c03f62850cb3c95c9f685b86c82302f3091
Prem chavhan <pchavhan@logiquad.com>
2023-09-18 20:27:51 +0530

    fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cp tools/docker/images/cells/* ./
 mv dockerfile Dockerfile
-sed -i "s~ARG version~ARG version=4.2.4~g" Dockerfile
+sed -i "s~ARG version~ARG version=4.2.7~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/cells:latest | docker load
