e8cf5f5fe9b327001ac45433ed2624ed4f650329
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-19 17:37:12 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -14,12 +14,3 @@ dbtcpname: cells
 dbtcpuser: pydio
 dbtcppassword: $MYSQL_PASSWORD
 EOT
-
-mkdir -p ./cellsdir
-chown -R 1000:1000 ./cellsdir
-
-mkdir -p ./install-conf.yml
-chown -R 1000:1000 ./install-conf.yml
-
-mkdir -p ./mysqldir
-chown -R 1000:1000 ./mysqldir
