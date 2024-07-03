7f5800ec966d361021cf1350562589586345d0e9
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-03 10:24:26 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cp tools/docker/images/cells/* ./
 mv dockerfile Dockerfile
-sed -i "s~ARG version~ARG version=4.2.7~g" Dockerfile
+
+latest_tag=$(curl -s https://api.github.com/repos/pydio/cells/tags | grep -m 1 '"name"' | sed -E 's/.*"v([^"]+)".*/\1/')
+
+
+sed -i "s~ARG version~ARG version=${latest_tag}~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/cells:latest | docker load
